a9be1b24d0616456609276513df6fa69c38212f5
feehnscmnt <felipenascimento411@gmail.com>
2025-04-09 16:11:46 -0300

    Inclusão de novas configurações.

--- src/main/java/br/com/basicauth/controller/BasicAuthController.java
+++ src/main/java/br/com/basicauth/controller/BasicAuthController.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
 import br.com.basicauth.message.ResponseMessages;
 import org.springframework.http.ResponseEntity;
+import org.apache.logging.log4j.LogManager;
 import org.springframework.http.HttpStatus;
+import org.apache.logging.log4j.Logger;
 import br.com.basicauth.util.Constants;
 import java.io.Serializable;
 import java.util.Objects;
@@ -21,6 +23,7 @@ import java.util.Objects;
 @RestController
 @RequestMapping("/v1")
 public class BasicAuthController implements Serializable {
+	private static final Logger LOG = LogManager.getLogger(BasicAuthController.class.getName());
 	private static final long serialVersionUID = 2873755961998877849L;
 	
 	/**
@@ -35,18 +38,23 @@ public class BasicAuthController implements Serializable {
 	@GetMapping("/basic-auth")
 	public ResponseEntity<Object> basicAuth(@RequestParam String user, @RequestParam String pass) {
 		
+		LOG.info("Autenticando o usuário {}...", user);
+		
 		if (user.isEmpty() && pass.isEmpty()) {
 			
+			LOG.error(Constants.STATUS_MESSAGE_CONFLICT_AUTH);
 			return ResponseEntity.status(HttpStatus.CONFLICT).body(new ResponseMessages(Constants.STATUS_MESSAGE_CONFLICT_AUTH, HttpStatus.CONFLICT, HttpStatus.CONFLICT.value()));
 			
 		}
 		
 		if (!Objects.equals(user, "basicAuth") || !Objects.equals(pass, "basicAuth")) {
 			
+			LOG.error(Constants.STATUS_MESSAGE_CONFLICT_AUTH);
 			return ResponseEntity.status(HttpStatus.CONFLICT).body(new ResponseMessages(Constants.STATUS_MESSAGE_CONFLICT_AUTH, HttpStatus.CONFLICT, HttpStatus.CONFLICT.value()));
 			
 		}
 		
+		LOG.info(Constants.STATUS_MESSAGE_OK_AUTH);
 		return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessages(Constants.STATUS_MESSAGE_OK_AUTH, HttpStatus.OK, HttpStatus.OK.value()));
 		
 	}
